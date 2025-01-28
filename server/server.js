f0892afd15cadc264d1dade782a1983cc2be716c
Illia Holovan <ilia.holovan@gmail.com>
2025-01-28 19:55:33 +0200

    test

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const config = require('../config');
+const config = require('../config.js');
 
 const app = express();
 
@@ -13,17 +13,19 @@ const updateAuthCodeInConfig = (authCode) => {
 	try {
 		// Читаем содержимое config.js
 		let configContent = fs.readFileSync(configPath, 'utf-8');
-
+		console.log(configContent, '11111111111111');
 		// Обновляем поле `authCode`
 		configContent = configContent.replace(
 			/authCode: ['"`]?[^'"`,]+['"`]?,/,
 			`authCode: '${authCode}',`
 		);
-
+		console.log(configContent, '2222222222222222222');
 		// Записываем изменения обратно в файл
 		fs.writeFileSync(configPath, configContent, 'utf-8');
+		console.log(configContent, '333333333333333333');
 		console.log('✅ authCode успешно обновлён в config.js');
 		console.log(config);
+		console.log(configPath, 'ПУТЬ4444444444444444444444444');
 	} catch (error) {
 		console.error('❌ Ошибка при обновлении config.js:', error.message);
 	}
